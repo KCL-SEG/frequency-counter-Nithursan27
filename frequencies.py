6af3baa3e89730fe244e5f21c58d277cc57b3bec
Nithursan <nithursan.muraleetharan@kcl.ac.uk>
2023-09-28 13:39:41 +0100

    Add solution

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,13 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    while(len(items) != 0):
+        value = 0
+        key = str(items[0])
+        for i in items:
+            if(str(i) == key):
+                value += 1
+        frequencies[key] = value
+        for i in range(value + 1):
+            items = [i for i in items if str(i) != key]
     return frequencies
